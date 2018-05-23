ab0e55ce1a354a848e8a71373303615dc85b5586
hunterdudley <39503920+hunterdudley@users.noreply.github.com>
2018-05-22 22:24:19 -0600

    Add files via upload

---
+++ Documents/Python/Projects/Scraper/src/read_lw.py
@@ -0,0 +1,40 @@
+import csv
+import requests
+from BeautifulSoup import BeautifulSoup
+
+# Define states to scrape
+states = ["30","31"] # California, Colorado
+properties = []
+
+# Scrape from all states
+for state in states:
+	
+	# Scrape multiple pages
+	page = 1
+	while page <= 20:
+		url = 'https://www.landwatch.com/default.aspx?ct=R&type=5,{};13,12;268,6843&pg={}'.format(state,page)
+		page = page+1
+		response = requests.get(url)
+		html = response.content
+		soup = BeautifulSoup(html)
+		dataset = soup.find('div', attrs={'class': 'left resultscontainer'})
+
+		# Scrape titles of properties and append into list
+		props = []
+		for propName in dataset.findAll('div', attrs={'class': 'propName'}):
+		    name = str(propName.find('a').text.replace('&nbsp;', ''))
+		    name = name.replace('  ',', ')
+		    name = name.replace('$', ', $')
+		    name = name.replace(' Acres','')
+		    props.append(name)
+
+		# Split list into matrix
+		for name in props:
+			splitName = name.split(', ')
+			properties.append(splitName)
+
+# write to csv
+with open('./land.csv', 'wb') as outfile:
+    writer = csv.writer(outfile)
+    writer.writerow(["Size (Acres)","City","County","State","Price","Notes"])
+    writer.writerows(properties)
