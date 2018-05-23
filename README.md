20feb79823946e8e978b75baac4e65c9b33de8de
Unknown <danagness@gmail.com>
2018-05-23 10:36:31 -0700

    readme update

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # landwatch
 Web scraper tutorial: http://first-web-scraper.readthedocs.io/en/latest/
+
+
+Daniel notes 5/23:
+
+- site breaks after 100 pages, can't find further listings
+- should eventually focus on particular counties, not full states
+- individual listings are identified by pid ("property ID") which is uniquely identified
+- PID is an 8 digit number
+- individual listing urls look like: https://www.landwatch.com/Inyo-County-California-Land-for-sale/pid/25031823
+- based on source code, looks like PID unquely IDs listing across whole site, not just within county
+- IDEA: pull all of the PIDs from a given page. Also record the county and state of the listing
+       - then loop through each of the links
+- once we get into the listing details we can look at aspects listed under "Property Features":
+    - waterfront access
+    - city
+    - land type
+    - road frontage/surface
+    - trees
+    - views
+    - terrain
+    - utility access
+these will be critical to building any type of interesting model - since not all land is created equal, we only get
+    so far with price per acre
+
+
+LONG TERM:
+- i think evenually it would be cool to build a model where we build a valuation of the property and then compare it to
+    the listed price
+- figure out if we can get data on past sales
+- bring in zillow land listings
+- incorporate other datsets: weather, distance to airports/highways, historical sales prices
--- read_lw.py
+++ read_lw.py
@@ -3,38 +3,38 @@ import requests
 from BeautifulSoup import BeautifulSoup
 
 # Define states to scrape
-states = ["30","31"] # California, Colorado
+states = ["30", "31", "56", "41"]  # California, Colorado, Montana, Idaho
 properties = []
 
 # Scrape from all states
 for state in states:
-	
-	# Scrape multiple pages
-	page = 1
-	while page <= 20:
-		url = 'https://www.landwatch.com/default.aspx?ct=R&type=5,{};13,12;268,6843&pg={}'.format(state,page)
-		page = page+1
-		response = requests.get(url)
-		html = response.content
-		soup = BeautifulSoup(html)
-		dataset = soup.find('div', attrs={'class': 'left resultscontainer'})
 
-		# Scrape titles of properties and append into list
-		props = []
-		for propName in dataset.findAll('div', attrs={'class': 'propName'}):
-		    name = str(propName.find('a').text.replace('&nbsp;', ''))
-		    name = name.replace('  ',', ')
-		    name = name.replace('$', ', $')
-		    name = name.replace(' Acres','')
-		    props.append(name)
+    # Scrape multiple pages
+    page = 1
+    while page <= 30:
+        url = 'https://www.landwatch.com/default.aspx?ct=R&type=5,{};13,12;268,6843&pg={}'.format(state, page)
+        page = page + 1
+        response = requests.get(url)
+        html = response.content
+        soup = BeautifulSoup(html)
+        dataset = soup.find('div', attrs={'class': 'left resultscontainer'})
 
-		# Split list into matrix
-		for name in props:
-			splitName = name.split(', ')
-			properties.append(splitName)
+        # Scrape titles of properties and append into list
+        props = []
+        for propName in dataset.findAll('div', attrs={'class': 'propName'}):
+            name = str(propName.find('a').text.replace('&nbsp;', ''))  # type: str
+            name = name.replace('  ', ', ')
+            name = name.replace('$', ', $')
+            name = name.replace(' Acres', '')
+            props.append(name)
+
+        # Split list into matrix
+        for name in props:
+            splitName = name.split(', ')
+            properties.append(splitName)
 
 # write to csv
 with open('./land.csv', 'wb') as outfile:
     writer = csv.writer(outfile)
-    writer.writerow(["Size (Acres)","City","County","State","Price","Notes"])
+    writer.writerow(["Size (Acres)", "City", "County", "State", "Price", "Notes"])
     writer.writerows(properties)
